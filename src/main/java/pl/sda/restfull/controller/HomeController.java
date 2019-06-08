ca43bc18adebc9562510f04a4325ed7eb3800595
MikiKru <michal_kruczkowski@o2.pl>
2019-06-08 10:42:23 +0200

    rest v3

--- src/main/java/pl/sda/restfull/controller/HomeController.java
+++ src/main/java/pl/sda/restfull/controller/HomeController.java
@@ -1,30 +1,28 @@
 package pl.sda.restfull.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.restfull.model.User;
 
 @RestController                     // obsługa żądań http
 public class HomeController {
-
+    User user;
     @GetMapping("/hello")           // adres wywołujący metodę
     public String hello(){          // sygnatura metody
         return "hello world!";
     }
-
     @GetMapping("/hello/{name}")    // adres wywołujący metodę
     public String hello(@PathVariable String name){          // sygnatura metody
         return "hello "+name+"!";
     }
-
     @PostMapping("/register")    // adres wywołujący metodę
-    public User hello(@PathVariable String email,
-                        @PathVariable String password){          // sygnatura metody
-        User user = new User(email,password);
+    public User register(String email, String password){          // sygnatura metody
+        user = new User(email,password);
         System.out.println(user);
+        return user; }
+    @PutMapping("/confirm")
+    public User confirm(){
+        user.setActivity(true);
         return user;
     }
 
