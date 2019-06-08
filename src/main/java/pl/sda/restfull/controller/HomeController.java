3d369808b6d1986c74f248d08bcea959fd6fc82a
MikiKru <michal_kruczkowski@o2.pl>
2019-06-08 10:21:56 +0200

    rest v2

--- src/main/java/pl/sda/restfull/controller/HomeController.java
+++ src/main/java/pl/sda/restfull/controller/HomeController.java
@@ -3,7 +3,9 @@ package pl.sda.restfull.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.restfull.model.User;
 
 @RestController                     // obsługa żądań http
 public class HomeController {
@@ -16,6 +18,14 @@ public class HomeController {
     @GetMapping("/hello/{name}")    // adres wywołujący metodę
     public String hello(@PathVariable String name){          // sygnatura metody
         return "hello "+name+"!";
+    }
+
+    @PostMapping("/register")    // adres wywołujący metodę
+    public User hello(@PathVariable String email,
+                        @PathVariable String password){          // sygnatura metody
+        User user = new User(email,password);
+        System.out.println(user);
+        return user;
     }
 
 
--- src/main/java/pl/sda/restfull/model/User.java
+++ src/main/java/pl/sda/restfull/model/User.java
@@ -1,4 +1,22 @@
 package pl.sda.restfull.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String email;
+    private String password;
+    private LocalDateTime register_date = LocalDateTime.now();
+    private boolean activity = false;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
