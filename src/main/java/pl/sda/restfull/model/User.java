bb9381102bba80e7e538cc5c09d559585fb737bc
MikiKru <michal_kruczkowski@o2.pl>
2019-06-08 11:31:06 +0200

    rest_db v4

--- src/main/java/pl/sda/restfull/model/User.java
+++ src/main/java/pl/sda/restfull/model/User.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id                                             // PK
+    @GeneratedValue(strategy = GenerationType.AUTO) // AI
+    private Long id;
     private String email;
     private String password;
     private LocalDateTime register_date = LocalDateTime.now();
