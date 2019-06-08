58b9d5ec4b77177f4b7a30bb9b7b98bbdd05ccf4
MikiKru <michal_kruczkowski@o2.pl>
2019-06-08 10:03:56 +0200

    rest v1

--- src/main/java/pl/sda/restfull/controller/HomeController.java
+++ src/main/java/pl/sda/restfull/controller/HomeController.java
@@ -2,6 +2,7 @@ package pl.sda.restfull.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController                     // obsługa żądań http
@@ -11,4 +12,11 @@ public class HomeController {
     public String hello(){          // sygnatura metody
         return "hello world!";
     }
+
+    @GetMapping("/hello/{name}")    // adres wywołujący metodę
+    public String hello(@PathVariable String name){          // sygnatura metody
+        return "hello "+name+"!";
+    }
+
+
 }
---
+++ src/main/java/pl/sda/restfull/model/User.java
@@ -0,0 +1,4 @@
+package pl.sda.restfull.model;
+
+public class User {
+}
